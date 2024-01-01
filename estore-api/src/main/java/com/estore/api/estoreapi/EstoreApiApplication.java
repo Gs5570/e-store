5f451753cdf28e846c1138ec69cf6f3219e6d810
Galekwan Sango <galekwansango@gmail.com>
2023-12-31 22:44:58 -0500

    adding port

--- estore-api/src/main/java/com/estore/api/estoreapi/EstoreApiApplication.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/EstoreApiApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EstoreApiApplication {
 
+	public String PORT = System.getenv("PORT")
+
 	public static void main(String[] args) {
 		SpringApplication.run(EstoreApiApplication.class, args);
 	}
